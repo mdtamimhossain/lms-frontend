772778fc90fb6d42d0d6d69386a9cc7c2c709677
mdtamimhossain <mdtamimhossain628487@gmail.com>
2023-08-29 23:34:56 +0600

    responsive for tablet added

--- js/main.js
+++ js/main.js
@@ -3,3 +3,29 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('windows-scroll',window.scrollY>0)
 })
+//show hide faq ans
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open')
+        const icon=faq.querySelector('.faq_icon i');
+        if(icon.className ==='uil uil-plus')
+            icon.className='uil uil-minus';
+        else
+            icon.className ='uil uil-plus';
+    })
+});
+const menu=document.querySelector('.nav_menu');
+const openBtn=document.querySelector('.open-menu-btn');
+const closeBtn=document.querySelector('.close-menu-btn');
+
+openBtn.addEventListener('click',()=>{
+    menu.style.display='flex';   
+    closeBtn.style.display='inline-block';
+    openBtn.style.display='none';
+});
+closeBtn.addEventListener('click',()=>{
+    menu.style.display='none';
+    closeBtn.style.display='none';
+    openBtn.style.display='inline-block';
+})
